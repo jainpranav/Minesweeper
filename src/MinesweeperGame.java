7775176bd4c23ea5b404f5d36b609666af956663
jainpranav <pranav11075@iiitd.ac.in>
2015-03-31 11:39:49 +0530

    Create MinesweeperGame.java

---
+++ src/MinesweeperGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pranav Jain
+ */
+public class MinesweeperGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        new GUI("beginner", false);
+    }
+}
